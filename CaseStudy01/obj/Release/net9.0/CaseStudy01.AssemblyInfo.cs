060bd747c850e7fec0f29080bcd12166efa45e7e
Potaeeatsthis <whoareyou.top@gmail.com>
2026-08-12 23:25:37 +0700

    FEAT: Fix result

--- CaseStudy01/obj/Release/net9.0/CaseStudy01.AssemblyInfo.cs
+++ CaseStudy01/obj/Release/net9.0/CaseStudy01.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CaseStudy01")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d45915cc582bb4c0d8c7e246b4f51d95a6e353a5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+af5866c94cd982d825d63296206bfe7d55e8509c")]
 [assembly: System.Reflection.AssemblyProductAttribute("CaseStudy01")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CaseStudy01")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
